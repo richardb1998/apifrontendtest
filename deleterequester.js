97384e8185ee4cbfea1cbde9b25318408f34ea78
Richard Bowser <techdude154@gmail.com>
2021-03-22 22:12:45 -0400

    Added delete api request

---
+++ deleterequester.js
@@ -0,0 +1,12 @@
+class deleterequester {
+    async delete(url, username, password) {
+        const response = await fetch((url + "?username=" + username + "&password=" + password), {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'text/plain'
+            },
+            body: {'username':username,
+                   'password':password}
+        });
+    }
+}
